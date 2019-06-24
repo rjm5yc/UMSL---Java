56ddeebe507cb41f501b98218b451c31cddb2218
rjm5yc <52049460+rjm5yc@users.noreply.github.com>
2019-06-24 14:11:34 -0500

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class Test2 
+{
+
+	//while, do while, for, for-each
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		
+		{
+			System.out.println("Hello");
+			
+		}
+	
+	
+	}
+	
+	
+}
