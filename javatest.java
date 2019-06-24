8eaed7294cf13dd36ab7c249a50e8ad4a058f600
rjm5yc <52049460+rjm5yc@users.noreply.github.com>
2019-06-24 14:11:09 -0500

    Add files via upload

---
+++ javatest.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+public class javatest 
+{
+	// while, do while, for, for-each
+	public static void main(String[] args) 
+	{
+	
+		int i=1;
+		while(i<=5)
+	{
+		System.out.println("Hello");
+		i++;
+	}
+	
+	}
+}
