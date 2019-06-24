e5b428d17cb6daa3e09e19424b9d09d4b322a894
rjm5yc <52049460+rjm5yc@users.noreply.github.com>
2019-06-24 14:24:25 -0500

    Add files via upload

---
+++ continuee.java
@@ -0,0 +1,24 @@
+package javaprogram;
+
+public class continuee 
+{
+	public static void main(String[] args) 
+	{
+	
+		for(int i=1;i<=10;i++)
+		{
+			if(i>5)
+			{
+				break;
+			}
+				
+				
+			System.out.println("value is " + i);	
+		}
+	
+	
+	
+	
+	}
+	
+}
