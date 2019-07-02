5dcd2b7b9e3c671f295bda44dea6c1233e41db93
rjm5yc <52049460+rjm5yc@users.noreply.github.com>
2019-07-02 18:40:40 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,69 @@
+package hibernate.project.studentinfo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="STUDENT")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "ssn")
+	private int ssn;
+	
+	public Student() {
+	}
+	
+	public Student(String fname, String lname, int ssn) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.ssn = ssn;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+	
+	public int getSSN() {
+		return ssn;
+	}
+	
+	public void setSSN(int ssn) {
+		this.ssn = ssn;
+	}
+	
+}
