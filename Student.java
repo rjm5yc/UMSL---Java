d75fa064aaab597215567f783c54de50d5fd475d
rjm5yc <52049460+rjm5yc@users.noreply.github.com>
2019-07-08 20:33:50 -0500

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,67 +1,12 @@
-package hibernate.project.studentinfo;
+package com.learning.springframework;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="STUDENT")
 public class Student {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "ID")
-	private int id;
-	
-	@Column(name = "first_name")
-	private String firstName;
-	
-	@Column(name = "last_name")
-	private String lastName;
-	
-	@Column(name = "ssn")
-	private int ssn;
-	
-	public Student() {}
-	public Student(String fname, String lname, int ssn) {
-		this.firstName = fname;
-		this.lastName = lname;
-		this.ssn = ssn;
+	public void getName(){;
+		System.out.println("Rich Mehan");
 	}
-	
-	public int getId() {
-		return id;
+	public void getSSN(){;
+		System.out.println("642189473");
 	}
-	
-	public void setId( int id ) {
-		this.id = id;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName( String first_name ) {
-		this.firstName = first_name;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName( String last_name ) {
-		this.lastName = last_name;
-	}
-	
-	public int getSSN() {
-		return ssn;
-	}
-	
-	public void setSSN( int ssn ) {
-		this.ssn = ssn;
-	}
-	
+
 }
